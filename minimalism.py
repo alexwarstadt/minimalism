11c6569749c69c80cfcfd2f9cc5dd5e9ffbe9ff1
CHRIS\ctk463 <christianklehm@utexas.edu>
2018-06-09 08:03:02 -0400

    replaced xml lexicon, commented out main (still not made compatible with new lexicon)

--- minimalism.py
+++ minimalism.py
@@ -1,5 +1,12 @@
 from typing import *
 
+# for xml parsing of imported lexicon
+import os
+from xml.etree import ElementTree
+# for xml parsing of imported lexicon
+import os
+from xml.etree import ElementTree
+
 
 class Feature(object):
     def __init__(self):
@@ -12,12 +19,12 @@ class Syn_Feature(Feature):
         pass
 
 class Sem_Feature(Feature):
-    def __init__(self):
-        super(Syn_Feature, self).__init__()
-        pass
+    def __init__(self,label: str):
+        super(Sem_Feature, self).__init__()
+        self.label = label
 
 class Cat_Feature(Syn_Feature):
-    def __init__(self, label):
+    def __init__(self, label: str):
         super(Cat_Feature, self).__init__()
         self.label = label
         
@@ -26,7 +33,7 @@ class Cat_Feature(Syn_Feature):
         return rep
 
 class Sel_Feature(Syn_Feature):
-    def __init__(self, label):
+    def __init__(self, label: str):
         super(Sel_Feature, self).__init__()
         self.label = label
     
@@ -35,8 +42,8 @@ class Sel_Feature(Syn_Feature):
         return rep
 
 class Phon_Feature(Feature):
-    def __init__(self, value: str):
-        self.value = value
+    def __init__(self, label: str):
+        self.label = label
 
     def __str__(self):
         rep = self.value
@@ -59,8 +66,54 @@ class UniversalGrammar(object):
 
 
 class Lexicon(object):
-    def __init__(self, lex):
-        self.lex: Set[LexicalItem] = lex
+    def __init__(self):
+        self.lex: Set[LexicalItem] = set()
+        # get the file path to lexicon data
+        file_name = "lexicon.xml"
+        full_file = os.path.abspath(os.path.join("data", file_name))
+        # create xml tree
+        dom = ElementTree.parse(full_file)
+        # extract data and create lexical items
+        words = dom.findall('word')
+        for w in words:
+            # extract word data
+            phon = w.find('phon').text
+            cat = w.find('syn/cat').text
+            sel = w.find('syn/sel').text
+            sem = w.find('sem').text
+            # create phon features
+            phon = phon.strip()
+            phon_list = phon.split()
+            phon_set = set()
+            for f in phon_list:
+                new_feat = Phon_Feature(f)
+                phon_set.add(new_feat)
+            # create sem features
+            sem = sem.strip()
+            sem_list = sem.split()
+            sem_set = set()
+            for f in sem_list:
+                new_feat = Sem_Feature(f)
+                sem_set.add(new_feat)
+            # create cat features
+            cat = cat.strip()
+            cat_list = cat.split()
+            cat_set = set()
+            for f in cat_list:
+                new_feat = Cat_Feature(f)
+                cat_set.add(new_feat)
+            # create sel features
+            sel = sel.strip()
+            sel_list = sel.split()
+            sel_set = set()
+            for f in sel_list:
+                new_feat = Sel_Feature(f)
+                sel_set.add(new_feat)
+            syn_set = set()
+            syn_set.union(sel_set)
+            syn_set.union(cat_set)
+            new_word = LexicalItem(syn_set, sem_set, phon_set)
+            self.lex.add(new_word)
 
 class ILanguage(object):
     def __init__(self, lexicon, ug):
@@ -330,9 +383,20 @@ def first_stage(lexical_array):
 
 counter = 100
 
+# for xml parsing of imported lexicon
 
+# for xml parsing of imported lexicon
+# Chris: xml parsing done in the lexicon constructor
 
 def main():
+    # prints the phonological features of each member of the lexicon
+    lexicon = Lexicon()
+    print('The Lexicon:')
+    for w in lexicon.lex:
+        for f in w.phon:
+            print(f.label)
+
+    '''
     # Syntactic fragment
     ncat = Cat_Feature("N")
     nsel = Sel_Feature("N")
@@ -365,7 +429,7 @@ def main():
 
     while (True):
         derivation.derive()
-
+    '''
     
 
 main()
